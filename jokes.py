cae2e585bdeb95e2e49995780467d46a6b9553f3
kimanthi777 <89152809+kimanthi777@users.noreply.github.com>
2023-05-18 15:31:37 +0300

    Create jokes.py
    created a python jokes file

---
+++ jokes.py
@@ -0,0 +1,17 @@
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why don't skeletons fight each other? They don't have the guts!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "I'm reading a book about anti-gravity. It's impossible to put down!",
+    "Why don't eggs tell jokes? Because they might crack up!",
+    "What did one wall say to the other wall? I'll meet you at the corner!",
+    "Why did the bicycle fall over? It was two-tired!",
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "What do you call fake spaghetti? An impasta!",
+    "How do you organize a space party? You planet!",
+]
+
+random_joke = random.choice(jokes)
+print(random_joke)
